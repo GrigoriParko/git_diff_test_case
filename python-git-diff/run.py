b297fcd0b9ed11f43b42f09c015f7ae9ae3b5654
GrigoriParko <grigori.panciohin@parkopedia.com>
2021-10-28 16:21:32 +0300

    Fixed envs

--- python-git-diff/run.py
+++ python-git-diff/run.py
@@ -2,14 +2,8 @@ import os
 import subprocess
 
 
-base = os.getenv("BASE")
-head = os.getenv("HEAD")
-patterns = os.getenv("PATTERNS")
-ppds_py = os.getenv("PPDS_PY_DIR")
-
-
 if __name__ == "__main__":
     subprocess.run(
-        f"git diff --name-only origin/${head}",
+        "git diff --name-only origin/${GITHUB_HEAD_REF}",
         shell=True
     )
