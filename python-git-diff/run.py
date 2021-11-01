831705c04622042e143015f7aa189113ce0af247
GrigoriParko <grigori.panciohin@parkopedia.com>
2021-11-01 10:56:55 +0200

    convert context into json

--- python-git-diff/run.py
+++ python-git-diff/run.py
@@ -5,8 +5,8 @@ import json
 
 
 git_context = os.getenv("GITHUB_CONTEXT")
-git_context_json = json.loads(json.dumps(git_context))
-event = git_context_json["event"]
+git_context_json = json.loads(git_context)
+logging.info(f"context type: {type(git_context_json)}")
 
 if __name__ == "__main__":
     logging.info(f"""
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     """)
 
     subprocess.run(
-        f"echo {str(event)}",
+        f"echo {type(git_context_json)}",
         shell=True
     )
