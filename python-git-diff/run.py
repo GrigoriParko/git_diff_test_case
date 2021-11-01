dad7e507daaa75112a429e7978671082f40aa1ca
GrigoriParko <grigori.panciohin@parkopedia.com>
2021-11-01 09:45:37 +0200

    Added base ref

--- python-git-diff/run.py
+++ python-git-diff/run.py
@@ -1,11 +1,17 @@
 import os
 import subprocess
+import logging
 
 
 head_ref = os.getenv("HEAD_REF", "master")
+base_ref = os.getenv("BASE_REF", "origin")
 
 if __name__ == "__main__":
+    logging.info(f"""
+    GITHUB_HEAD_REF: {head_ref}
+    GITHUB_BASE_REF: {base_ref}
+    """)
     subprocess.run(
-        f"git diff --name-only origin/{head_ref}",
+        f"git diff --name-only `{base_ref}...{head_ref}`",
         shell=True
     )
