4d5ef51858705a897cdb1d5bf9462091d72c9f15
GrigoriParko <grigori.panciohin@parkopedia.com>
2021-11-01 17:09:30 +0200

    re default branch

--- python-git-diff/run.py
+++ python-git-diff/run.py
@@ -1,14 +1,16 @@
 import os
 import subprocess
 import json
+import re
 
 
 git_context = os.getenv("GITHUB_CONTEXT", "")
 git_context_json = json.loads(git_context)
+default_branch = re.match(".*default_branch: (.*),", git_context_json)
 
 if __name__ == "__main__":
 
     subprocess.run(
-        f'echo "::debug::{git_context}"',
+        f'echo "::debug::{default_branch}"',
         shell=True,
     )
