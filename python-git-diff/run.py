9f135840f5f4262153c123db520fa277da97aac5
GrigoriParko <grigori.panciohin@parkopedia.com>
2021-11-01 17:24:07 +0200

    regexp

--- python-git-diff/run.py
+++ python-git-diff/run.py
@@ -7,6 +7,8 @@ import re
 git_context = os.getenv("GITHUB_CONTEXT", "")
 git_context_json = json.loads(git_context)
 default_branch = re.match(".*default_branch.*: (.*),.*", str(git_context_json))
+if default_branch:
+    default_branch: str = default_branch.replace('"', '')
 
 if __name__ == "__main__":
 
