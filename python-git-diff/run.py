695bafecc7e63b0ec0d5a5462638ef528d15e307
GrigoriParko <grigori.panciohin@parkopedia.com>
2021-11-01 16:40:27 +0200

    echo context

--- python-git-diff/run.py
+++ python-git-diff/run.py
@@ -1,17 +1,14 @@
 import os
 import subprocess
-from io import StringIO
 import json
 
 
 git_context = os.getenv("GITHUB_CONTEXT", "")
-io = StringIO(git_context)
-git_context_json = json.load(io)
-context_type = type(git_context_json)
+git_context_json = json.loads(git_context)
 
 if __name__ == "__main__":
 
     subprocess.run(
-        f"echo {context_type}",
+        f"echo {git_context[:50]}",
         shell=True
     )
