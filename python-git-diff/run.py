beda9615b1a0df5871adf89fa11cbc563913d4f6
GrigoriParko <grigori.panciohin@parkopedia.com>
2021-11-01 17:36:49 +0200

    regexp

--- python-git-diff/run.py
+++ python-git-diff/run.py
@@ -6,13 +6,13 @@ import re
 
 git_context = os.getenv("GITHUB_CONTEXT", "")
 git_context_json = json.loads(git_context)
-default_branch_match = re.match(r".*default_branch.*: (.*),.*", str(git_context_json))
-if default_branch_match:
-    default_branch = default_branch_match[0].replace('"', '')
+# default_branch_match = re.match(r".*default_branch.*: (.*),.*", str(git_context_json))
+# if default_branch_match:
+#     default_branch = default_branch_match[0].replace('"', '')
 
 if __name__ == "__main__":
 
     subprocess.run(
-        f'echo "::debug::{default_branch}"',
+        f'echo "::debug::{str(git_context_json)}"',
         shell=True,
     )
